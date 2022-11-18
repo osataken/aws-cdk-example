af2505882e48b126180fff2e009b2091aeb3ecc4
Kunaruk Osatapirat <osataken@amazon.co.th>
2022-11-18 13:06:53 +0700

    Initial commit with ECS Fargate example

--- app.py
+++ app.py
@@ -3,26 +3,12 @@ import os
 
 import aws_cdk as cdk
 
-from aws_cdk_example.aws_cdk_example_stack import AwsCdkExampleStack
+from ecs_fargate import ECSFargateStack
 
 
 app = cdk.App()
-AwsCdkExampleStack(app, "AwsCdkExampleStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+# Uncomment to try different example construct
+# ECSFargateStack(app, "ECSFargateStack")
 
 app.synth()
---
+++ ecs_fargate/ecs_fargate_stack.py
@@ -0,0 +1,70 @@
+from aws_cdk import (
+    Stack,
+    SecretValue,
+    CfnOutput,
+    aws_ec2 as ec2, 
+    aws_ecs as ecs,
+    aws_ecr as ecr,
+    aws_rds as rds,
+    aws_ecs_patterns as ecs_patterns
+)
+from constructs import Construct
+
+class ECSFargateStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Get existing VPC from VPC ID
+        vpc = ec2.Vpc.from_lookup(self, "VPC",
+            vpc_id = "vpc-04d7b77ca873c8525" # << customize VPC ID here
+        )
+        cluster = ecs.Cluster(self, "ECSFargate-Cluster", vpc=vpc)
+        
+        # Create RDS Aurora DB Cluster
+        db_user = "root" #modify username here
+        db_password = "examplepassword" #modify password here
+        db_name = "sample_db" #modify database name here
+
+        db_cluster = rds.DatabaseCluster(self, "Database",
+            engine=rds.DatabaseClusterEngine.aurora_mysql(version=rds.AuroraMysqlEngineVersion.VER_2_08_1),
+            credentials=rds.Credentials.from_username(
+                username=db_user, 
+                password=SecretValue.unsafe_plain_text(db_password)),
+            default_database_name=db_name,
+            instance_props=rds.InstanceProps(
+                instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
+                vpc_subnets=ec2.SubnetSelection(
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+                ),
+                vpc=vpc
+            )
+        )
+        
+        # Create ECS Fargate Cluster
+        ecr_repository = "sample_ecr_repo" #modify ECR repository here
+
+        repository = ecr.Repository.from_repository_name(self, construct_id, ecr_repository)
+        image = ecs.ContainerImage.from_ecr_repository(repository, "latest")
+        load_balanced_fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(self, "FlightSpecialsService",
+            cluster=cluster,            # Required
+            cpu=512,                    # Default is 256
+            desired_count=2,            # Default is 1
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                container_name="flightspecials",
+                environment= { 
+                    "DB_HOST" : db_cluster.cluster_read_endpoint.hostname,
+                    "DB_USERNAME" : db_user,
+                    "DB_PASSWORD" : db_password
+                },
+                image=image, 
+                container_port=8080),
+            memory_limit_mib=2048,      # Default is 512
+            public_load_balancer=False)  # Default is True
+            
+        load_balanced_fargate_service.target_group.configure_health_check(
+            path="/" #modify healthcheck path here
+        )
+        db_cluster.connections.allow_default_port_from(load_balanced_fargate_service.service);
+        
+        CfnOutput(self, 'Flight DB Read Endpoint', value=db_cluster.cluster_read_endpoint.hostname)
